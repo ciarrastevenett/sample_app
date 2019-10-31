5f715edbf6e2f2ad23820a4fd5bd88447f37a7d2
Ciarrra Stevenett <ciarra.stevenett@gmail.com>
2019-10-31 17:27:35 -0600

    Implement basic login

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,7 +7,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 end
 
   test "should get home" do
-    get static_pages_home_url
+    get home_path
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
